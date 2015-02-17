af81302a0000c82efd81a73281c16ab39d3408e3
Alex Hill <alex@mako.local>
2015-02-17 02:57:33 -0500

    add sleep/wakeup to link

--- link.sh
+++ link.sh
@@ -5,5 +5,7 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/zprofile ~/.zprofile
+ln -s ~/.dotfiles/sleep ~/.sleep
+ln -s ~/.dotfiles/wakeup ~/.wakeup
 
 mkdir ~/.vimswap
